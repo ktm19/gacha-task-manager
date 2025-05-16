8dd5ad1df04dcf5b0d857a4fdd887c95d937892d
unknown <katiemin24@gmail.com>
2025-05-15 17:08:36 -0700

    connect frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -16,20 +16,35 @@ This program is free software: you can redistribute it and/or modify
   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ========================================================== */
-
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import React from 'react';
 import { createEngine } from 'express-react-views';
 //import bcrypt from 'bcrypt'
-import connection from './database.js'
+import connection from './database.js';
 
 console.log("poggers");
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = join(dirname(__filename), '..');
 console.log(__dirname);
 const app = express();
+//app.use(cors());
+const corsOption = {
+    credentials: true,
+    origin: ['http://localhost:5173']
+}
+
+app.use(cors(corsOption));
+
+//cors({origin: true})
+
+
+var corsOptions = {
+    origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 // for static stuff like HTML files
 app.use(express.static(join(__dirname, 'public')));
@@ -42,83 +57,84 @@ app.engine('jsx', createEngine());
 
 app.get('/', (req,res) => res.send('Try: /status, /users, or /tasks/2') );
 
-app.get('/status', (req, res) => res.send('Success.') );
+app.get('/status', function (req, res, next) { res.send('Success.') } );
 
 app.get('/users', (req, res) => {
-	connection.query(
-		"SELECT * FROM `main_db`.`users`",
-		(error, results, fields) => {
-			if(error) throw error;
-			res.json(results);
-		}
-	);
+        connection.query(
+                "SELECT * FROM `main_db`.`users`",
+                (error, results, fields) => {
+                        if(error) throw error;
+                        res.json(results);
+                }
+        );
 });
 
 app.post('/register', async (req, res) => {
-	const {username, password} = req.body;
-	try {
-		//NOTE: CURRENTLY NOT HASHING. NEED TO IMPORT BCRYPT
-		/*const hashedPassword = await bcrypt.hash(password, 10);*/
-		const query = 'INSERT INTO users (username, password, money, pity) VALUE (?, ?, 0, 0)';
-		//TO DO: implement check if username is already taken
-		connection.query(query, [username, password], (err,result) => {if (err) throw err;
-			res.status(201).send('User registered successfully: ' + username);
-		});
-	} catch (error) {
-		res.status(500).send('Error registering user');
-	}
+        const {username, password} = req.body;
+        try {
+                //NOTE: CURRENTLY NOT HASHING. NEED TO IMPORT BCRYPT
+                /*const hashedPassword = await bcrypt.hash(password, 10);*/
+                const query = 'INSERT INTO users (username, password, money, pity) VALUE (?, ?, 0, 0)';
+                //TO DO: implement check if username is already taken
+                connection.query(query, [username, password], (err,result) => {if (err) throw err;
+                        res.status(201).send('User registered successfully: ' + username);
+                });
+        } catch (error) {
+                res.status(500).send('Error registering user');
+        }
 });
 
 app.post('/login', (req,res) => { 
-	const {username, password} = req.body;
+        const {username, password} = req.body;
 
-	//find user by username
-	const query = 'SELECT * FROM users WHERE username = ?';
-	connection.query(query, [username], async(err, result) => {
-		if (err) throw err;
-		if (results.length > 0) { //CAN CHANGE BEHAVIOR IF TAKEN
-			const user = results[0];
-			//to do: implement hash
-			/**const ismatch = await bcrypt.compare(password, user.password);
-			 * if (isMatch)
-			 */
-			if (user.password == password) {
-				res.status(200).send('Login successful');
-			} else {
-				res.status(401).send('Invalid credentials');
-			}
-		} else {
-			res.status(404).send('User not found :(');
-		}
-	});
+        //find user by username
+        const query = 'SELECT * FROM users WHERE username = ?';
+        connection.query(query, [username], async(err, result) => {
+                if (err) throw err;
+                if (results.length > 0) { //CAN CHANGE BEHAVIOR IF TAKEN
+                        const user = results[0];
+                        //to do: implement hash
+                        /**const ismatch = await bcrypt.compare(password, user.password);
+                         * if (isMatch)
+                         */
+                        if (user.password == password) {
+                                res.status(200).send('Login successful');
+                        } else {
+                                res.status(401).send('Invalid credentials');
+                        }
+                } else {
+                        res.status(404).send('User not found :(');
+                }
+        });
 });
 
-app.get('/sus', (req, res) => {
-	res.send('ඞ')
+app.get('/sus', cors(corsOptions), (req, res, next) => {
+        res.send('ඞ')
 });
 
 app.get('/pog', (req, res) => {
-	res.sendFile(join(__dirname, 'get-it-done-gacha', 'public', 'pog.html'));
+        res.sendFile(join(__dirname, 'get-it-done-gacha', 'public', 'pog.html'));
 });
 
 app.get('/supersus', (req, res) => {
-	res.render('sus', { title: 'ඞ' });
+        res.render('sus', { title: 'ඞ' });
 });
 
 app.route('/tasks/:task_id')
 .get( (req, res, next) => {
-	connection.query(
-		"SELECT * FROM `main_db`.`tasks` WHERE task_id = ?", req.params.task_id,
-		(error, results, fields) => {
-			if(error) throw error;
-			res.json(results);
-		}
-	);
+        connection.query(
+                "SELECT * FROM `main_db`.`tasks` WHERE task_id = ?", req.params.task_id,
+                (error, results, fields) => {
+                        if(error) throw error;
+                        res.json(results);
+                }
+        );
 });
 
 // Use port 8080 by default, unless configured differently in Google Cloud
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
-   	console.log(`App is running at: http://0.0.0.0:${port}`);
+        console.log(`App is running at: http://0.0.0.0:${port}`);
 });
+
 
