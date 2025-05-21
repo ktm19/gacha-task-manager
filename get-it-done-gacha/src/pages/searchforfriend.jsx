d9bf6c3fe94ccb3c7f4f25a948ef25cef74409c7
unknown <katiemin24@gmail.com>
2025-05-21 15:37:12 -0700

    make databaseComponents folder, work on searching for friends

--- get-it-done-gacha/src/pages/searchforfriend.jsx
+++ get-it-done-gacha/src/pages/searchforfriend.jsx
@@ -3,36 +3,41 @@ import React, { useState, useEffect } from 'react';
 import '../App.css' 
 import TextFieldSubmit from '../textFieldSubmit.jsx';
 import { Link } from 'react-router-dom';
+import SearchForUser from '../databaseComponents/searchforuser.jsx';
 import axios from 'axios';
 
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 axios.defaults.baseURL = 'http://localhost:8080';
 
 function SearchForFriend() {
-    const [results, setResults] = useState([]);
-  
-    useEffect(() => {
-      axios.get("/searchForFriend?username=test").then((response) => {
-        setResults(response.data);
-      });
-    }, []);
-    console.log(results);
-    console.log(results["username"]);
+  const [showSearchResults, setShowSearchResults] = useState(false);
+  const [userToSearchFor, setUserToSearchFor] = useState("");
 
-    console.log("Search for friends page loaded");
+  const handleClick = (values) => {
+    setShowSearchResults(true); // Update state to render the component
+    setUserToSearchFor(values[0]);
+    console.log(userToSearchFor);
+  };
+
+  const test = (userObj) => {
+    console.log("got username: " + userObj["username"]);
+  }
+
+  console.log("Search for friends page loaded");
   return (
     <div className = "p-4 justify-center items-center flex flex-col h-screen bg-gray-100">
+      {showSearchResults && <SearchForUser username={userToSearchFor} toDo={test}/>}
       <h1 className = "text-xl font-bold mb-4"> Login </h1>
       <TextFieldSubmit 
         numFields={1} 
-        onSubmit={(values) => alert(`Submitted`)} 
+        onSubmit={(values) => handleClick(values)} 
         fieldPlaceholders={['Username']}
       />
       <div className="mt-4">
         <p className="text-sm">
-          Don't have an account?{' '}
-          <Link to="/register" className="text-blue-500 hover:underline">
-            Register here
+          Want to go back?{' '}
+          <Link to="/" className="text-blue-500 hover:underline">
+            Home
           </Link>
         </p>
         </div>
